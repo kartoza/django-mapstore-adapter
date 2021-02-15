54ca343e69751c915aa83ec456ee8a472343e52d
afabiani <alessio.fabiani@geo-solutions.it>
2021-02-15 16:31:17 +0100

    - Align the MapStoreResource user with the current Map owner (in order to allow the new owner to edit the map)
    (cherry picked from commit 53c8e7c16d50917542cffc327698d226c69a3d6e)

--- mapstore2_adapter/__init__.py
+++ mapstore2_adapter/__init__.py
@@ -28,9 +28,13 @@ def fixup_map(map_id):
     for _m in Map.objects.filter(id=map_id):
         try:
             _u = get_user_model().objects.get(username=_m.owner.username)
-            _mm, _created = MapStoreResource.objects.get_or_create(id=_m.id, user_id=_u.id)
-            if _created:
-                _mm.save()
+            _mm = MapStoreResource.objects.filter(id=_m.id)
+            if _mm.count():
+                _mm = _mm.get()
+            else:
+                _mm = MapStoreResource.objects.create(id=_m.id, user_id=_u.id)
+            _mm.user = _u
+            _mm.save()
         except Exception as e:
             logger.exception(e)
 
